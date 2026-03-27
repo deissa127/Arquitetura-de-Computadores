8adc9293929533914d61e1ef47d69a56dd7af2a2
Hadassa dos Santos Medeiros <Hadassadosmedeiros@gmail.com>
2026-03-27 20:09:30 +0000

    q15

--- Atividades Arq. de comp./Lista 1/Q15.asm
+++ Atividades Arq. de comp./Lista 1/Q15.asm
@@ -12,7 +12,7 @@ main:
 
         div $6, $5
         mflo $7
-        mfhi $10
+        mfhi $11
 
         div $7, $5
         mflo $7
@@ -21,5 +21,45 @@ main:
         div $7, $5
         mflo $7
         mfhi $9
-
         
+        div $7, $5
+        mfhi $8
+        
+        # milhar
+        sub $12, $0, $8
+        srl $12, $12, 31
+        sll $12, $12, 4
+        addi $4, $8, 32
+        add $4, $4, $12
+        addi $2, $0, 11
+        syscall
+        
+        # centena
+        sub $13, $0, $9
+        srl $13, $13, 31
+        sll $13, $13, 4
+        addi $4, $9, 32
+        add $4, $4, $13
+        addi $2, $0, 11
+        syscall
+        
+        # dezena
+        sub $14, $0, $10
+        srl $14, $14, 31
+        sll $14, $14, 4
+        addi $4, $10, 32
+        add $4, $4, $14
+        addi $2, $0, 11
+        syscall
+        
+        # unidade
+        sub $15, $0, $11
+        srl $15, $15, 31
+        sll $15, $15, 4
+        addi $4, $11, 32
+        add $4, $4, $15
+        addi $2, $0, 11
+        syscall
+        
+        addi $2, $0, 10
+        syscall
