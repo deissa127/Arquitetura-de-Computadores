736a544ab42ba983a7d85f38d30a5f8daef444e0
Hadassa dos Santos Medeiros <Hadassadosmedeiros@gmail.com>
2026-04-25 19:12:31 +0000

    q08l3

--- Atividades Arq. de comp./Lista 3/Q08.asm
+++ Atividades Arq. de comp./Lista 3/Q08.asm
@@ -2,47 +2,57 @@
 #    valor digitado e o maior valor digitado. O programa informa o maior e o menor, um em
 #    cada linha, quando o usuário digitar um 0.
 
-# obs.; o mips não guarda uma lista, então é preciso comparar toda vez que entrar um novo número.
-# erro - o menor número consta como 0
-
-
 .text
 main:
-	addi $10, $0, 1 # menor numero
-#----------CORPO DO LAÇO--------------
-laco:	
-	add $2, $0, 5
-	syscall
-	add $8, $0, $2 # maior numero até então
+# Lê o primeiro número para inicializar maior e menor
+    addi $v0, $0, 5
+    syscall
+    add $8, $0, $v0         # $8 = número lido
 
-teste:	# é maior que 0 ?
-	slt $9, $8, $10		# $a > $b = $x -> 0 // $a < $b = $x -> 1
-	bne $9, $0, fim		# nao é maior que 0 - fim
+    beq $8, $0, fim         # Se já começar com 0, vai pro fim
 
-comp:	# era maior que 0
-	slt $25, $8, $10	# $a > $b = $x -> 0 // $a < $b = $x -> 1 // 0 = maior, 1 = menor
-	beq $25, $0, maior
-	
-menor:
-	add $19, $0, $8	# menor numero
-maior:
-	add $18, $0, $8	# maior numero
-#-------FIM DO CORPO DO LAÇO--------------      
-prox:	
-	j laco 
-	
-fim:	
-	add $4, $0, $18
-	add $2, $0, 1
-	syscall
-	
-	add $4, $0, ' '
-	add $2, $0, 11
-	syscall
-	
-	add $4, $0, $19
-	add $2, $0, 1
-	syscall
-	
-	add $2, $0, 10
-	syscall
+    add $18, $0, $8         # maior = primeiro número
+    add $19, $0, $8         # menor = primeiro número
+
+# ----------CORPO DO LAÇO--------------
+laco:
+    addi $v0, $0, 5
+    syscall
+    add $8, $0, $v0         # $8 = número lido
+
+teste:
+    beq $8, $0, fim         # Se digitou 0, encerra
+
+comp_maior:
+    slt $25, $18, $8        # $25 = 1 se maior_atual < novo número
+    beq $25, $0, comp_menor # Não é maior, testa se é menor
+    add $18, $0, $8         # Atualiza maior
+    j prox
+
+comp_menor:
+    slt $25, $8, $19        # $25 = 1 se novo número < menor_atual
+    beq $25, $0, prox       # Não é menor, próxima iteração
+    add $19, $0, $8         # Atualiza menor
+
+#-------FIM DO CORPO DO LAÇO--------------
+prox:
+    j laco
+
+fim:
+    # Imprime maior
+    add $4, $0, $18
+    addi $v0, $0, 1
+    syscall
+
+    addi $4, $0, '\n'
+    addi $v0, $0, 11
+    syscall
+
+    # Imprime menor
+    add $4, $0, $19
+    addi $v0, $0, 1
+    syscall
+
+    # Encerra
+    addi $v0, $0, 10
+    syscall
