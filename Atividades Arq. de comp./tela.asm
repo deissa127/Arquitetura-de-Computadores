7aecffab36381519fec9ced00fdb2c1bece2a5fc
Hadassa dos Santos Medeiros <Hadassadosmedeiros@gmail.com>
2026-05-26 21:01:21 +0000

    extra

--- Atividades Arq. de comp./tela.asm
+++ Atividades Arq. de comp./tela.asm
@@ -54,16 +54,26 @@ fim:
 
 
 
-	.text
+# o picel 32256
+# total de pixels = 8192
+# linha = 128
+# coluna = 64
+
+
+
+.text
 main:
 	lui $8, 0x1001
+	lui $7, 0x1001
+	addi $7, $7, 32256
 	lui $6, 0x1001
 	lui $5, 0x1001
 	
-	lui $9, 0x00ff
+	li $9, 0xfa5a9a
 	
 	addi $10, $0, 128
 	addi $11, $0, 8064
+	addi $12, $0, 0
 
 linha:
 	beq $10, $0, linha2
@@ -73,11 +83,24 @@ linha:
 	j linha
 	
 linha2:
-	beq $11, 8196, fim
-	sw $9, 0($8)
-	addi $8, $8, 4
-	addi $10, $10, 1	
-	j linha
+	beq $11, 8192, colunas
+	sw $9, 0($7)
+	addi $7, $7, 4
+	addi $11, $11, 1	
+	j linha2
+	
+colunas:
+	beq $12, 32256, parede
+	sw $9, 0($6)
+	addi $6, $6, 508
+	addi $12, $12, 508
+	sw $9, 0($6)
+	addi $6, $6, 4
+	addi $12, $12, 4
+	j colunas
+	
+parede:
+	sw $9, 110($5)
 	
 fim:
 	addi $2, $0, 10
